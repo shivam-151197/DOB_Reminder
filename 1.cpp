9e93f2b18988a3b6b3c943ddad34cb26a5e401ac
Shivam Rajpoot <shivamrajpoot199@gmail.com>
2020-10-04 23:29:25 +0530

    Java is the best

---
+++ 1.cpp
@@ -0,0 +1,22 @@
+#inlcude<bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+void solve(){
+	int ar[1001] = {0};
+	for(int i = 0; i < 1001; i++){
+		cin>>ar[i];
+	}
+	sort(ar, ar+1000);
+	
+}
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
